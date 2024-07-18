4d436343116fb10d0d95518d0241affa1ab4e074
Adane-Dane <cocadane4@gmail.com>
2024-07-18 08:22:54 +0300

    cart done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -10,7 +10,11 @@ import { DataContext } from "../DataProvider/DataProvider";
 const Header = () => {
 
   const [{basket},dispatch]=useContext(DataContext)
-  console.log(basket.length);
+
+  const totalItem =basket?.reduce((amount,item)=>{
+    return item.amount + amount
+  },0)
+  // console.log(basket.length);
 
   return (
     <>
@@ -64,7 +68,7 @@ const Header = () => {
               </Link>
               <Link to="/cart" className={classes.cart}>
                 <BiCart size={35} />
-                <span>{basket.length}</span>
+                <span>{totalItem}</span>
               </Link>
             </div>
           </div>
