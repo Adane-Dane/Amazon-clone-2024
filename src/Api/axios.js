7bbb83ff2d29404cab2981092a5129caec929aef
Adane-Dane <cocadane4@gmail.com>
2024-07-25 20:12:41 +0300

    final

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://127.0.0.1:5001/clone-2024-8cd75/us-central1/api",
+  // local
+  // baseURL: "http://127.0.0.1:5001/clone-2024-8cd75/us-central1/api",
+
+  // on render.com 
+  baseURL: "https://amazon-clone-api-14oy.onrender.com/",
 });
 
 export { axiosInstance };
